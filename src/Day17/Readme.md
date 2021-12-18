c594d27f5135c6aa134300d8bc0df277ac646833
Drew Haven <drew.haven@gmail.com>
2021-12-18 15:47:52 -0800

    Day 17: Solution

--- src/Day17/Readme.md
+++ src/Day17/Readme.md
@@ -15,3 +15,41 @@ Also, it appears that all the target areas are below the origin line.  This may 
 So how do we find the maximum height?  We want the maximum velocity that will put us in the target area in a single step without overshooting.  The higher that velocity, the higher the corresponding initial velocity and the higher the probe will have gone.  That will correspond to an initial velocity of `target_ymin + 1`.  Remember, the target number is negative so it's min instead of max and plus insteaad of minus. The height of the probe is another triangle number, just like for the horizontal, so the height will be `v_y0 * (v_y0 + 1) / 2`.
 
 At least in my case I calculated this and it worked out great!
+
+Part 2 is just a bit more fiddly.  I took the approach of calculating the iterations on which different `v_y` values intersect the target area.  I put these in a map keyed on which iterations they are valid for.  That way I just have to iterate a bunch of `v_x` values and can pull the `v_y` values that they need to pair with based on what iterations they are crossing the target area.  Don't forget to deduplicate because one pair may cross the target area twice!
+
+## Optimization
+
+I was afraid that this problem would take too long to run.  For large inputs that is definitely a concern.  Given that numbers are on the order of 200 we only have to try about 40,000 distinct pairs even if we brute forced it.  With the algoritm above it's much closer to `max_y * (max_x - min_x)` because we do Xs and Ys separately and have only to look up a subset of Xs.  It's more complicated because the map has logarithmic insertion, but the point is that it's less than the full product.
+
+Anyway, the runtime is small enough that I don't think it's worth optimizing at this point.
+
+```
+Parsing input...
+benchmarking...
+time                 417.3 ns   (416.1 ns .. 418.7 ns)
+                     1.000 R²   (1.000 R² .. 1.000 R²)
+mean                 415.5 ns   (414.2 ns .. 417.8 ns)
+std dev              5.652 ns   (3.913 ns .. 9.222 ns)
+variance introduced by outliers: 13% (moderately inflated)
+
+================================================================================
+Running Part 1...
+benchmarking...
+time                 6.706 ns   (6.675 ns .. 6.745 ns)
+                     1.000 R²   (1.000 R² .. 1.000 R²)
+mean                 6.791 ns   (6.749 ns .. 6.841 ns)
+std dev              157.8 ps   (141.3 ps .. 176.1 ps)
+variance introduced by outliers: 38% (moderately inflated)
+
+Part 1: 7626
+================================================================================
+Running Part 2...
+benchmarking...
+time                 1.042 ms   (1.036 ms .. 1.049 ms)
+                     1.000 R²   (0.999 R² .. 1.000 R²)
+mean                 1.050 ms   (1.045 ms .. 1.057 ms)
+std dev              19.42 μs   (16.07 μs .. 22.88 μs)
+
+Part 2: 2032
+```
--- src/Day17/Solution.hs
+++ src/Day17/Solution.hs
@@ -1,14 +1,208 @@
 module Solution where
 
 import qualified Data.Attoparsec.Text as P
+import qualified Data.List as List
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Exercise
 
-type Input = String 
+data Input = Input
+  { minX :: Int,
+    maxX :: Int,
+    minY :: Int,
+    maxY :: Int
+  }
+  deriving (Show)
+
+inputParser :: P.Parser Input
+inputParser = do
+  "target area: x="
+  minX <- P.signed P.decimal
+  ".."
+  maxX <- P.signed P.decimal
+  ", y="
+  minY <- P.signed P.decimal
+  ".."
+  maxY <- P.signed P.decimal
+  return $ Input {minX, maxX, minY, maxY}
+
+highestAltitude :: Input -> Int
+highestAltitude Input {minY} =
+  let vy = negate minY
+   in vy * (vy - 1) `div` 2
+
+indexed = zip [1 ..]
+
+-- | Creates a map of VY values that land in the target on various iteration counts.
+possibleVY :: Input -> Map Int [Int]
+possibleVY Input {minY, maxY} =
+  Map.fromListWith (++) . concatMap targetYs $ [minY .. (negate minY - 1)]
+  where
+    ys vy0 =
+      map (\(i, _) -> (i, vy0))
+        . dropWhile (\(i, (y, vy)) -> y > maxY)
+        . indexed
+        . takeWhile (\(y, vy) -> y >= minY)
+        . iterate (\(y, vy) -> (y + vy, vy - 1))
+        $ (0, vy0)
+    targetYs = map (\(i, vy) -> (i, [vy])) . ys
+
+possibleSolutions :: Input -> Map Int [Int] -> Set (Int, Int)
+possibleSolutions Input {minX, maxX} vyMap =
+  Set.fromList $ concatMap vxSolutions [1 .. maxX]
+  where
+    (maxI, _) = Map.findMax vyMap
+    xsIndexed vx =
+      map (\(i, _) -> (i, vx))
+        . dropWhile (\(i, (x, vx)) -> x < minX)
+        . takeWhile (\(i, (x, vx)) -> x <= maxX && i <= maxI)
+        . indexed
+        . iterate (\(x, vx) -> (x + vx, if vx > 0 then vx - 1 else 0))
+        $ (0, vx)
+    vxSolutions vx =
+      concatMap
+        ( \(i, vx) ->
+            map (\vy -> (vx, vy)) . Map.findWithDefault [] i $ vyMap
+        )
+        . xsIndexed
+        $ vx
+
+allSolutions :: Input -> Set (Int, Int)
+allSolutions input =
+  possibleSolutions input (possibleVY input)
 
 solution :: Solution Input
 solution =
   Solution
-    { solutionParser = P.many1 P.anyChar,
-      solutionPart1 = const $ (124 * 123) `div` 2,
-      solutionPart2 = undefined
+    { solutionParser = inputParser,
+      solutionPart1 = highestAltitude,
+      solutionPart2 = Set.size . allSolutions
     }
+
+exampleInput :: Input
+exampleInput =
+  Input
+    { minX = 20,
+      maxX = 30,
+      minY = -10,
+      maxY = -5
+    }
+
+exampleSolutions :: Set (Int, Int)
+exampleSolutions =
+  Set.fromList
+    [ (23, -10),
+      (25, -9),
+      (27, -5),
+      (29, -6),
+      (22, -6),
+      (21, -7),
+      (9, 0),
+      (27, -7),
+      (24, -5),
+      (25, -7),
+      (26, -6),
+      (25, -5),
+      (6, 8),
+      (11, -2),
+      (20, -5),
+      (29, -10),
+      (6, 3),
+      (28, -7),
+      (8, 0),
+      (30, -6),
+      (29, -8),
+      (20, -10),
+      (6, 7),
+      (6, 4),
+      (6, 1),
+      (14, -4),
+      (21, -6),
+      (26, -10),
+      (7, -1),
+      (7, 7),
+      (8, -1),
+      (21, -9),
+      (6, 2),
+      (20, -7),
+      (30, -10),
+      (14, -3),
+      (20, -8),
+      (13, -2),
+      (7, 3),
+      (28, -8),
+      (29, -9),
+      (15, -3),
+      (22, -5),
+      (26, -8),
+      (25, -8),
+      (25, -6),
+      (15, -4),
+      (9, -2),
+      (15, -2),
+      (12, -2),
+      (28, -9),
+      (12, -3),
+      (24, -6),
+      (23, -7),
+      (25, -10),
+      (7, 8),
+      (11, -3),
+      (26, -7),
+      (7, 1),
+      (23, -9),
+      (6, 0),
+      (22, -10),
+      (27, -6),
+      (8, 1),
+      (22, -8),
+      (13, -4),
+      (7, 6),
+      (28, -6),
+      (11, -4),
+      (12, -4),
+      (26, -9),
+      (7, 4),
+      (24, -10),
+      (23, -8),
+      (30, -8),
+      (7, 0),
+      (9, -1),
+      (10, -1),
+      (26, -5),
+      (22, -9),
+      (6, 5),
+      (7, 5),
+      (23, -6),
+      (28, -10),
+      (10, -2),
+      (11, -1),
+      (20, -9),
+      (14, -2),
+      (29, -7),
+      (13, -3),
+      (23, -5),
+      (24, -8),
+      (27, -9),
+      (30, -7),
+      (28, -5),
+      (21, -10),
+      (7, 9),
+      (6, 6),
+      (21, -5),
+      (27, -10),
+      (7, 2),
+      (30, -9),
+      (21, -8),
+      (22, -7),
+      (24, -9),
+      (20, -6),
+      (6, 9),
+      (29, -5),
+      (8, -2),
+      (27, -8),
+      (30, -5),
+      (24, -7)
+    ]
